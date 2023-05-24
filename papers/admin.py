f41443c9cad0930a2f81c75ba88988e41733af08
William Qian <william.z.qian@gmail.com>
2023-05-24 16:12:32 +0800

    Update paper database

--- papers/admin.py
+++ papers/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Paper, PapersDetail
+
 
 # Register your models here.
+
+class PaperDetailInline(admin.TabularInline):
+    model = PapersDetail
+    extra = 1
+
+
+class PaperAdmin(admin.ModelAdmin):
+    inlines = [PaperDetailInline]
+    list_display = ('paper_name', 'date', 'paper_link')
+
+
+admin.site.register(Paper, PaperAdmin)
--- papers/models.py
+++ papers/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from projects.models import Project
+
 
 # Create your models here.
+
+class Paper(models.Model):
+    paper_name = models.CharField(max_length=100, null=True, default=None)
+    date = models.DateField()
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    paper_link = models.CharField(max_length=100, null=True, default=None, blank=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.paper_name
+
+
+class PapersDetail(models.Model):
+    patent = models.ForeignKey(Paper, on_delete=models.CASCADE)
+    people = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=True)
