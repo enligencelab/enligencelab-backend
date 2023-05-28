ed64527273280cfd8b492ac7430fdb3af7a87874
William Qian <william.z.qian@gmail.com>
2023-05-28 19:09:39 +0800

    API Update

--- people/urls.py
+++ people/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import PeopleViewSet
 
 urlpatterns = [
-    path('teachers', PeopleViewSet.as_view({'get': 'get_teachers'})),
-    path('graduates', PeopleViewSet.as_view({'get': 'get_graduates'})),
-    path('undergraduates', PeopleViewSet.as_view({'get': 'get_undergraduates'})),
+    path('teachers/', PeopleViewSet.as_view({'get': 'get_teachers'})),
+    path('graduates/', PeopleViewSet.as_view({'get': 'get_graduates'})),
+    path('undergraduates/', PeopleViewSet.as_view({'get': 'get_undergraduates'})),
 ]
